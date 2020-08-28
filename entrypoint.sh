6c74c9ff6a11baf9710cd96d166a7f3b819e2d52
Ryan <rchard@anl.gov>
2020-08-27 20:54:42 -0500

    Makes interchange port range configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting Forwarder against REDIS server $REDIS_HOST : $REDIS_PORT"
-forwarder-service -a $ADVERTISED_FORWARDER_ADDRESS -p 8080 --redishost $REDIS_HOST --redisport $REDIS_PORT
+forwarder-service -a $ADVERTISED_FORWARDER_ADDRESS -p 8080 --redishost $REDIS_HOST --redisport $REDIS_PORT --min_ic_port $MIN_IC_PORT --max_ic_port $MAX_IC_PORT
 
