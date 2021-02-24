1dabc43cdbb1afe3f27c297816644e547bd05989
Yadu Nand Babuji <yadudoc1729@gmail.com>
2021-02-24 17:12:23 -0600

    Split off wait_for_redis into a separate file, and updated entrypoint to use set -ex to exit early and be more verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
+set -ex
+
 python3 -c "import funcx; print('funcx Version : ', funcx.__version__)"
 python3 -c "import funcx_endpoint; print('funcx_endpoint Version : ', funcx_endpoint.__version__)"
 python3 -c "import funcx_forwarder; print('funcx_forwarder Version : ', funcx_forwarder.__version__)"
 
-cat <<EOF > wait_for_redis.py
-import redis
-import time
-import os
-
-hostname=os.environ.get('REDIS_HOST')
-port=os.environ.get('REDIS_PORT')
-connected=False
-print(f"Attempting to connect to REDIS at {hostname}:{port}")
-for i in range(10):
-    try:
-        redis_client = redis.StrictRedis(host=hostname, port=port, decode_responses=True)
-        redis_client.ping()
-    except Exception:
-        print("Trying")
-        time.sleep(6)
-    else:
-        print("Connected!")
-        connected=True
-        break
-if not connected:
-    print("Failed to connect to REDIS with 10 attempts over 60s")
-    exit(-1)
-EOF
+if [[ -z "${REDIS_HOST}" ]]; then
+    REDIS_HOST="$FUNCX_REDIS_MASTER_SERVICE_HOST"
+fi
 
 python3 wait_for_redis.py
-
-echo "Starting Forwarder against REDIS server $REDIS_HOST : $REDIS_PORT"
-# forwarder-service -a $ADVERTISED_FORWARDER_ADDRESS -p 8080 --redishost $REDIS_HOST --redisport $REDIS_PORT
-# YADU: REMOVE DEBUG FLAG FOR PRODUCTION:
 
 if [[ -z "${ADVERTISED_FORWARDER_ADDRESS}" ]]; then
     ADVERTISED_FORWARDER_ADDRESS=`wget http://169.254.169.254/latest/meta-data/public-ipv4; cat public-ipv4`
 fi
-
-echo "Advertisting forwarder address: $ADVERTISED_FORWARDER_ADDRESS"
 
 forwarder-service -a $ADVERTISED_FORWARDER_ADDRESS -p 8080 --redishost $REDIS_HOST --redisport $REDIS_PORT -d --stream_logs
 
