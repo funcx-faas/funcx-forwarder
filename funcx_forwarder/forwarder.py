168f9dd152d8da8d5cad5ca03f10ac6d64f8e651
Loonride <22580625+Loonride@users.noreply.github.com>
2021-07-26 15:10:05 -0500

    change task transition to log type field

--- funcx_forwarder/forwarder.py
+++ funcx_forwarder/forwarder.py
@@ -302,7 +302,7 @@ class Forwarder(Process):
             "function_id": task.function_id,
             "endpoint_id": task.endpoint,
             "container_id": task.container,
-            "task_transition": True
+            "type": "task_transition"
         }
         logger.info(transition_name, extra=extra_logging)
 
