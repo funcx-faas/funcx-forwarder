55e5cdcea9223cfe52d07093f76aa2e8f6661705
EricLee543 <408228228@qq.com>
2021-11-19 10:08:47 +0800

    re-run work flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 if [[ -z "${REDIS_HOST}" ]]; then
     REDIS_HOST="$FUNCX_REDIS_MASTER_SERVICE_HOST"
 fi
+
 echo "Starting Forwarder against REDIS server $REDIS_HOST : $REDIS_PORT"
 forwarder-service -a $ADVERTISED_FORWARDER_ADDRESS -p 8080 --redishost $REDIS_HOST --redisport $REDIS_PORT --min_ic_port $MIN_IC_PORT --max_ic_port $MAX_IC_PORT
 
